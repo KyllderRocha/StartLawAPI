44340ba70512ecc14a836e7ff0334bfb1ceef53b
Kyllder Rocha <kyllder.rocha@gmail.com>
2022-06-27 18:45:06 -0300

    Adicionando controller do usuario

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { UsuarioService } from './usuario.service';
 
 @Controller('usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
+  @Post()
+  create(@Body() createUsuarioDto: Prisma.UsuarioCreateInput) {
+    return this.usuarioService.create(createUsuarioDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usuarioService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usuarioService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUsuarioDto: Prisma.UsuarioUpdateInput) {
+    return this.usuarioService.update(+id, updateUsuarioDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usuarioService.remove(+id);
+  }
 }
